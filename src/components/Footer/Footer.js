df813b1d45338e860c5ae68ae6151fe8e9a2f169
saifyahya <saifbarakat991@gmail.com>
2023-07-11 13:04:15 +0000

    style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
+import "./Footer.css"
 function Footer() {
     return(
         <div>
-            <a href="https://www.linkedin.com/in/saif-yahya-276306217/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/saif-yahya-276306217/">Linkedin</a><br/>
             <a href="saifbarakat991@gmail.com">Send Email</a>
             <p>Copyright &copy; Saif Yahya</p>
         </div>
