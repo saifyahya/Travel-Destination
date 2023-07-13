34ca28e22c17ad423bb06c6d3854b89f9df089d9
saifyahya <saifbarakat991@gmail.com>
2023-07-13 16:20:42 +0000

    map tour inside tours

--- src/components/TourDetails/TourDetails.js
+++ src/components/TourDetails/TourDetails.js
@@ -4,7 +4,7 @@ import data from '../../data/db.json'
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 function TourDetails() {
-    const {id} = useParams();
+const {id} = useParams();
 let x = data.filter(ele =>ele.id==id)
 const [expanded, setExpanded] = useState(false);
 
@@ -18,7 +18,7 @@ return(
     <h3>{x[0].name}</h3>
     <img src={x[0].image} alt={x[0].name} />
     <p>{x[0].price}</p>
-    <p>{expanded?`${x[0].info.slice(0, 200)}`:x[0].info}</p>
+    <p>{expanded?`${x[0].info.slice(0, 200)}...`:x[0].info}</p>
     <button onClick={infoHandler}>{expanded?"see more":"see less"}</button>
     <Footer/>
     </>
--- src/components/Tours/Tour/Tour.js
+++ src/components/Tours/Tour/Tour.js
@@ -1,19 +1,10 @@
-import { Link } from "react-router-dom"
 import './Tour.css'
 function Tour(props) {
     return (
         <div className="tour-container">
-        {
-            props.tour.map((obj, i) => (
-            <div key={i} className="tour">
-            <Link to={`/city/${obj.id}`}>
-            <h3>{obj.name}</h3>
-            <img src={obj.image} alt={obj.name} />
-            </Link>
-          </div>
-        ))
-    }
-    </div>
-    )
+            <h3>{props.tour.name}</h3>
+            <img src={props.tour.image} alt={props.tour.name} />
+        </div>
+        )
 }
 export default Tour
